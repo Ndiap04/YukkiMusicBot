070fe64e8b4c259a4bde50aad429e10d2753e60e
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-12-15 10:16:40 +0700

    Update playlist.py

--- Yukki/Database/playlist.py
+++ Yukki/Database/playlist.py
@@ -2,33 +2,33 @@ from typing import Dict, List, Union
 
 from Yukki import db
 
-playlistdb_lofi = db.playlistlofi
+playlistdb_dangdut = db.playlistdangdut
+playlistdb_reggae = db.playlistreggae
+playlistdb_indopop = db.playlistindopop
 playlistdb_rock = db.playlistrock
-playlistdb_sad = db.playlistsad
-playlistdb_party = db.playlistparty
-playlistdb_bollywood = db.playlistbollywood
-playlistdb_hollywood = db.playlisthollywood
-playlistdb_punjabi = db.playlistpunjabi
-playlistdb_others = db.playlistothers
+playlistdb_hiphop = db.playlisthiphop
+playlistdb_alquran = db.playlistalquran
+playlistdb_djlokal = db.playlistdjlokal
+playlistdb_kristen = db.playlistkristen
 
 
 async def _get_playlists(chat_id: int, type: str) -> Dict[str, int]:
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
+    if type == "Dangdut":
+        xd = playlistdb_dangdut
+    elif type == "Reggae":
+        xd = playlistdb_reggae
+    elif type == "IndoPop":
+        xd = playlistdb_indopop
+    elif type == "Rock":
         xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
-        xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
+    elif type == "HipHop":
+        xd = playlistdb_hiphop
+    elif type == "Al-Qur'an":
+        xd = playlistdb_alquran
+    elif type == "DjLokal":
+        xd = playlistdb_djlokal
+    elif type == "Kristen":
+        xd = playlistdb_kristen
     _notes = await xd.find_one({"chat_id": chat_id})
     if not _notes:
         return {}
@@ -57,22 +57,22 @@ async def save_playlist(chat_id: int, name: str, note: dict, type: str):
     name = name
     _notes = await _get_playlists(chat_id, type)
     _notes[name] = note
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
+    if type == "Dangdut":
+        xd = playlistdb_dangdut
+    elif type == "Reggae":
+        xd = playlistdb_reggae
+    elif type == "IndoPop":
+        xd = playlistdb_indopop
+    elif type == "Rock":
         xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
-        xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
+    elif type == "HipHop":
+        xd = playlistdb_hiphop
+    elif type == "Al-Qur'an":
+        xd = playlistdb_alquran
+    elif type == "DjLokal":
+        xd = playlistdb_djlokal
+    elif type == "Kristen":
+        xd = playlistdb_kristen
     await xd.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
@@ -81,22 +81,22 @@ async def save_playlist(chat_id: int, name: str, note: dict, type: str):
 async def delete_playlist(chat_id: int, name: str, type: str) -> bool:
     notesd = await _get_playlists(chat_id, type)
     name = name
-    if type == "Lofi":
-        xd = playlistdb_lofi
-    elif type == "Weeb":
+    if type == "Dangdut":
+        xd = playlistdb_dangdut
+    elif type == "Reggae":
+        xd = playlistdb_reggae
+    elif type == "IndoPop":
+        xd = playlistdb_indopop
+    elif type == "Rock":
         xd = playlistdb_rock
-    elif type == "Sad":
-        xd = playlistdb_sad
-    elif type == "Party":
-        xd = playlistdb_party
-    elif type == "Bollywood":
-        xd = playlistdb_bollywood
-    elif type == "Hollywood":
-        xd = playlistdb_hollywood
-    elif type == "Punjabi":
-        xd = playlistdb_punjabi
-    elif type == "Others":
-        xd = playlistdb_others
+    elif type == "HipHop":
+        xd = playlistdb_hiphop
+    elif type == "Al-Qur'an":
+        xd = playlistdb_alquran
+    elif type == "DjLokal":
+        xd = playlistdb_djlokal
+    elif type == "Kristen":
+        xd = playlistdb_kristen
     if name in notesd:
         del notesd[name]
         await xd.update_one(
